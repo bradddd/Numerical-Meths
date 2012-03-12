547959e5faa64d978250b78acd104b0d52b6ef57
Brad <brad.matthiesen@gmail.com>
2012-03-11 21:39:32 -0500

    changed main setup to allow easier setting of higher n

--- mglin/main.c
+++ mglin/main.c
@@ -5,14 +5,16 @@ int main(int argc, char **argv){
   int i,j,k;
   FILE *outfile;
   double ***f;
-  int n=17;
+  int n=129;
   int ncycle=2;
   f = d3tensor(1,n,1,n,1,n);
   //for (i=1;i<n;++i)
   //    for (j=1;j<n;++j)
   //        for (k=1;k<n;++k)
   //            f[i][j][k] = 0.0;
-  f[8][8][8]=1.0;
+  
+  int c = n/2 + 1;
+  f[c][c][c]=1.0;
   mglin(f,n,ncycle);
   outfile = fopen("soln.dat", "w");
   fwrite(&f[1][1][1],sizeof(double),n*n*n,outfile);
